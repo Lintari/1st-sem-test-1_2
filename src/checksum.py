3136461e3386dea36c77ac3d1465f50e9b275d3c
Luda Ashikhmina <ludaemalah@gmail.com>
2025-11-07 13:27:15 +0000

    рассмотрела случай когда последний элемент Х

--- src/checksum.py
+++ src/checksum.py
@@ -1,6 +1,13 @@
 def modulo11Checksum(ISBNNumber: str):
 
     digits = [int(char) for char in ISBNNumber if char.isdigit()]
+    for char in ISBNNumber:
+	if char.isdigit():
+	    digit.append(int(char))
+	elif (len(digit) == 9) and char == 'X':
+	    digits.append(10)
+	    
+
     if len(digits) != 10:
 	return False
      
