c927cbf7ef29649a12f10902053bcaa0b7a14e15
Luda Ashikhmina <ludaemalah@gmail.com>
2025-11-07 13:10:44 +0000

    проверяю длину, что она 10

--- src/checksum.py
+++ src/checksum.py
@@ -1,7 +1,9 @@
 def modulo11Checksum(ISBNNumber: str):
 
     digits = [int(char) for char in ISBNNumber if char.isdigit()]
-
+    if len(digits) != 10:
+	return False
+     
     checkDigit = digits[-1]
 
     total = 0
