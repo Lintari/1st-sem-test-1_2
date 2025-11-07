8b652f51c2d761b427cfc4c862f67ccd0d50b0ab
Luda Ashikhmina <ludaemalah@gmail.com>
2025-11-07 13:55:55 +0000

    added and fixed tests

--- test/test_checksum.py
+++ test/test_checksum.py
@@ -1,9 +1,10 @@
 from src.checksum import modulo11Checksum
 
 
-def test_good():
-    assert modulo11Checksum("2-266-11156-8")
-
-
 def test_bad():
-    assert not modulo11Checksum("2-266-11156-3")
+    assert not  modulo11Checksum("2-266-11156-8")
+    assert not modulo11Checksum("1-563-9873X-X")
+
+def test_good():
+    assert  modulo11Checksum("2-266-11156-3")
+    assert  modulo11Checksum("1-563-68745-X")
